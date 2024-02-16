d5a2df71a0b34d6ad2bd88ee35ec22ce7d2df59a
ManiSamet <msa366@sfu.ca>
2024-02-16 01:12:37 +0000

    q3

--- Q3/Eval.cpp
+++ Q3/Eval.cpp
@@ -16,30 +16,103 @@ using std::endl;
 using std::cin;
 
 
-int main () {
-    Scanner S(cin);
-    Token t;
+int OperationPriority(Token operation) {
 
-    Stack<Token> numstack, opstack;  // 2x Stacks of type Token
-    
-
-    t = S.getnext();
-
-    // Pretty printer coding demo.  Please remove before coding
-    while (t.tt != eof) {
-        if (t.tt == integer || t.tt == lptok || t.tt == rptok) {
-            cout << t;
-        } else if (t.tt == pltok || t.tt == mitok || 
-                   t.tt == asttok || t.tt == slashtok) {
-            cout << ' ' << t << ' ';
+    if (operation.tt == asttok || operation.tt == slashtok){ // if * or / higher pressedance
+        return 2;
         }
 
-        t = S.getnext();
+    else if (operation.tt == pltok || operation.tt == mitok){ // + or - lower pressedance
+        return 1;
     }
+    
+    return 0;
+}
 
-    cout << endl;
-    // End pretty printer coding demo.
+
+
+int getOperationVal(Token operation, int num1, int num2){
+
+    if (operation.tt == pltok) // + addition
+        return num1 + num2;
+
+    if (operation.tt == mitok) // - subtraction
+        return num1 - num2;
+
+    if (operation.tt == asttok) // * multiplication
+        return num1 * num2;
+
+    if (operation.tt == slashtok) // / division
+        return num1 / num2;
 
     return 0;
 }
 
+
+
+int main() {
+
+    Scanner S = cin; // object of class Scanner with parameter cin
+    Token t;
+    Stack<Token> number_stack, operator_stack; // stacks to hold numbers and operators
+    t = S.getnext();
+
+    while (t.tt != eof) {
+
+        if (t.tt == integer){
+            number_stack.push(t);
+        }
+
+        else if (t.tt == lptok){
+            operator_stack.push(t);
+
+        }
+        else if (t.tt == rptok) {
+            while (!operator_stack.isEmpty() && operator_stack.peek().tt != lptok) {
+                Token operation = operator_stack.pop();
+                Token num2 = number_stack.pop();
+                Token num1 = number_stack.pop();
+
+                int result = getOperationVal(operation, num1.val, num2.val);
+                Token resultToken;
+                resultToken.tt = integer;
+                resultToken.val = result;
+                number_stack.push(resultToken);
+            }
+            operator_stack.pop(); // pops the left parenthesis from opstack
+        }
+
+        else if (t.tt == pltok || t.tt == mitok || t.tt == asttok || t.tt == slashtok) {
+            while (!operator_stack.isEmpty() && OperationPriority(operator_stack.peek()) >= OperationPriority(t)) {
+                Token operation = operator_stack.pop();
+                Token num2 = number_stack.pop();
+                Token num1 = number_stack.pop();
+                int result = getOperationVal(operation, num1.val, num2.val);
+                Token resulttok;
+                resulttok.tt = integer;
+                resulttok.val = result;
+                number_stack.push(resulttok);
+            }
+            operator_stack.push(t); // push the current operator in opstack
+        }   
+
+    t = S.getnext(); //get the next token
+    }
+
+    while (!operator_stack.isEmpty()){
+
+        Token operation = operator_stack.pop();
+        Token num2 = number_stack.pop();
+        Token num1 = number_stack.pop();
+        int result = getOperationVal(operation, num1.val, num2.val);
+        Token resulttok;
+        resulttok.tt = integer;
+        resulttok.val = result;
+        number_stack.push(resulttok);
+    }
+
+    if (!number_stack.isEmpty()){
+        cout << number_stack.peek().val << endl;
+    }
+    return 0;
+}
